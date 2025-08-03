0b51c7c6ee4ec0f864de2a83550ff91ba5b54878
MarlonSantos321 <202407257119@alunos.estacio.br>
2025-08-03 11:02:54 -0300

    TrabalhoSuperTrunfo1.c

--- TrabalhoSuperTrunfo1.c
+++ TrabalhoSuperTrunfo1.c
@@ -1,16 +1,15 @@
 //Nivel Novato
-
 #include <stdio.h>
 
 int main() {
     // Dados para a Carta 1
     char Estado1[25], Cidade1[30], Codigo1[10];
-    int População1, Pontos1;
+    int Populacao1, Pontos1;
     float Pib1, Densidade1, Per_Capita1, Area1;
 
     // Dados para a Carta 2
     char Estado2[25], Cidade2[30], Codigo2[10];
-    int População2, Pontos2;
+    int Populacao2, Pontos2;
     float Pib2, Densidade2, Per_Capita2, Area2;
 
     // Dados da Carta 1
@@ -28,8 +27,8 @@ int main() {
     printf("Area da Cidade: ");
     scanf("%f", &Area1);
 
-    printf("População: ");
-    scanf("%d", &População1);
+    printf("Populacao: ");
+    scanf("%d", &Populacao1);
 
     printf("Pib Da Cidade: ");
     scanf("%f", &Pib1);
@@ -52,8 +51,8 @@ int main() {
     printf("Area da Cidade: ");
     scanf("%f", &Area2);
 
-    printf("População: ");
-    scanf("%d", &População2);
+    printf("Populacao: ");
+    scanf("%d", &Populacao2);
 
     printf("Pib Da Cidade: ");
     scanf("%f", &Pib2);
@@ -62,12 +61,12 @@ int main() {
     scanf("%d", &Pontos2);
 
     // Calculando a Densidade Populacional
-    Densidade1 = População1 / Area1;
-    Densidade2 = População2 / Area2;
+    Densidade1 = Populacao1 / Area1;
+    Densidade2 = Populacao2 / Area2;
 
     // Calculando o PIB per capita
-    Per_Capita1 = Pib1 / População1;
-    Per_Capita2 = Pib2 / População2;
+    Per_Capita1 = Pib1 / Populacao1;
+    Per_Capita2 = Pib2 / Populacao2;
 
     // Variáveis para contar as vitórias
     int vitoriasCidade1 = 0;
@@ -80,54 +79,66 @@ int main() {
     if (Densidade1 < Densidade2) {
         printf("A cidade %s tem uma densidade populacional menor que a cidade %s\n", Cidade1, Cidade2);
         vitoriasCidade1++;
-    } else {
+    } else if (Densidade1 > Densidade2) {
         printf("A cidade %s tem uma densidade populacional menor que a cidade %s\n", Cidade2, Cidade1);
         vitoriasCidade2++;
+    } else {
+        printf("As cidades %s e %s têm a mesma densidade populacional\n", Cidade1, Cidade2);
     }
 
     // Comparação do PIB per capita (quem tem maior PIB per capita ganha)
     if (Per_Capita1 > Per_Capita2) {
         printf("A cidade %s tem um PIB per capita maior que a cidade %s\n", Cidade1, Cidade2);
         vitoriasCidade1++;
-    } else {
+    } else if (Per_Capita1 < Per_Capita2) {
         printf("A cidade %s tem um PIB per capita maior que a cidade %s\n", Cidade2, Cidade1);
         vitoriasCidade2++;
+    } else {
+        printf("As cidades %s e %s têm o mesmo PIB per capita\n", Cidade1, Cidade2);
     }
 
     // Comparação dos Pontos Turísticos (quem tem mais pontos turísticos ganha)
     if (Pontos1 > Pontos2) {
         printf("A cidade %s tem mais pontos turísticos que a cidade %s\n", Cidade1, Cidade2);
         vitoriasCidade1++;
-    } else {
+    } else if (Pontos1 < Pontos2) {
         printf("A cidade %s tem mais pontos turísticos que a cidade %s\n", Cidade2, Cidade1);
         vitoriasCidade2++;
+    } else {
+        printf("As cidades %s e %s têm o mesmo número de pontos turísticos\n", Cidade1, Cidade2);
     }
 
     // Comparação da População (quem tem mais população ganha)
-    if (População1 > População2) {
+    if (Populacao1 > Populacao2) {
         printf("A cidade %s tem uma população maior que a cidade %s\n", Cidade1, Cidade2);
         vitoriasCidade1++;
-    } else {
+    } else if (Populacao1 < Populacao2) {
         printf("A cidade %s tem uma população maior que a cidade %s\n", Cidade2, Cidade1);
         vitoriasCidade2++;
+    } else {
+        printf("As cidades %s e %s têm a mesma população\n", Cidade1, Cidade2);
     }
 
     // Comparação da Área (quem tem mais área ganha)
     if (Area1 > Area2) {
         printf("A cidade %s tem uma área maior que a cidade %s\n", Cidade1, Cidade2);
         vitoriasCidade1++;
-    } else {
+    } else if (Area1 < Area2) {
         printf("A cidade %s tem uma área maior que a cidade %s\n", Cidade2, Cidade1);
         vitoriasCidade2++;
+    } else {
+        printf("As cidades %s e %s têm a mesma área\n", Cidade1, Cidade2);
     }
 
     // Comparação do PIB (quem tem mais PIB ganha)
     if (Pib1 > Pib2) {
         printf("A cidade %s tem um PIB maior que a cidade %s\n", Cidade1, Cidade2);
         vitoriasCidade1++;
-    } else {
+    } else if (Pib1 < Pib2) {
         printf("A cidade %s tem um PIB maior que a cidade %s\n", Cidade2, Cidade1);
         vitoriasCidade2++;
+    } else {
+        printf("As cidades %s e %s têm o mesmo PIB\n", Cidade1, Cidade2);
     }
 
     // Determinando a cidade vencedora
@@ -137,9 +148,10 @@ int main() {
     } else if (vitoriasCidade1 < vitoriasCidade2) {
         printf("A cidade vencedora é %s com %d vitórias!\n", Cidade2, vitoriasCidade2);
     } else {
-        printf("As cidades empataram! Cada uma teve %d vitórias.\n", vitoriasCidade1);
+        printf("As cidades empataram!");
     }
 
     return 0;
 }
+
 
